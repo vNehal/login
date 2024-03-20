1d8438707d6cc4c8de2e66c5f51b5355bc46a898
Sivakumar Manoharan <siva.ms2000@gmail.com>
2024-03-19 21:44:52 -0400

    [FIX]: API changes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import './App.css';
 import { LoginSignup } from './components/LoginSignup/LoginSignup';
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Navigate to="/mydashboard" replace />} />
-            <Route
-                path="/login"
-                element={
-                  <>
-                      <LoginSignup/>
-                  </>
-                  }
-              />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<><LoginSignup/></>} />
       </Routes>
     </BrowserRouter>
+    // <div>
+    //   <LoginSignup/>
+    // </div>
   );
 }
 
